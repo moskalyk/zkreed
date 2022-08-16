b968b7abbcc6cb9d1f9c6e98989547626360ccaf
moskalyk <morgan.moskaly@protonmail.ch>
2022-08-15 22:56:15 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# zkReed
+
+zkreed is a proof of concept tarot game, assembled to numeric arithmetic signifiers for inspection. like "counting cards" for tarots.
+
+oracles can peek into subtle energetic variations from cryptograhically proven pre-compiled circuits
+where for 9 card readings, oracle's card placements become adjusted for equilibrium testing
+
+e.g
+
+| [ -2 ] | _3_ | [  ] | *			| [ 1 ] | _3_ | [  ] | 
+|  _1_   | ___ |  __  |		vs		|  _1_  | ___ |  __  |  
+| [   ]  | ___ | [  ] |				| [   ] | ___ | [  ] |
+
+querents can share numerology essence, without revealing cards. 
+
+before posting this, i rolled a 5 with my 20-sided dice, representing energies from:
+- heirophant
+- pentagram
+- team
+- 5 elements
---
+++ Reed.ts
@@ -0,0 +1,50 @@
+import {
+  Field,
+  State,
+  PublicKey,
+  SmartContract,
+  state,
+  method,
+  Bool,
+  Circuit,
+  Signature,
+  Permissions,
+  DeployArgs,
+} from 'snarkyjs';
+
+// conserve future energy with present energy bitfield translation, test with fool
+const tarot = [
+  ['x','o','o','o','o','o','o','o','o','o','o','o','x','o','o','o','o','o','o','o','o','o','o','o','o','o','o','o','o']
+]
+
+/*
+  1-4
+    - elements - +1 fire, -1 water, +2 air, -2 earth
+  1-10
+    - planets - 1-5-10 -> -5-+5
+  1-12 = 
+    - zodiac - compatibility chart: +0 match, +1 favourable, -1 not favourable
+  1-3 = 1x, 2x, 3x, rate (?)
+    -north node
+    -star of david
+    -pentagram
+*/
+
+// loop on element, if el == x, then el.assertEquals().add(sum)
+
+// [,,,]
+
+// TODO: apply matrix multiplication against prime frequencies.
+// const primes = [
+//   '',
+// ]
+
+export class Reed extends SmartContract {
+	@state(Field) spread = State<Field>();
+
+	@method pull(){
+
+	}
+	// get card, loop on index of list of elements, compute dot product to running sum
+	// make sure to do indexed pulls on, what does this mean?
+}
